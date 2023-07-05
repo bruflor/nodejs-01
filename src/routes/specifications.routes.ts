b54bb5a45915effd58ace44810d38fd34392bcf8
Bruna <brn.flor@gmail.com>
2023-07-05 20:12:28 +0100

    specifications route

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,7 +1,20 @@
 import { Router } from "express";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { SpecificationRepository } from "../modules/cars/repositories/SpecificationRepository";
 
 const specificationRoutes = Router();
 
-specificationRoutes.post("/");
+const specificationRepository = new SpecificationRepository();
+
+specificationRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  const createSpecificationService = new CreateSpecificationService(
+    specificationRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
 
 export { specificationRoutes };
