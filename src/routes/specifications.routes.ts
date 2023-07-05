197753601dbd844e688a21e140239ff2a8a582c4
Bruna <brn.flor@gmail.com>
2023-07-05 20:59:37 +0100

    specification implement and useCase

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,20 +1,12 @@
 import { Router } from "express";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
-import { SpecificationRepository } from "../modules/cars/repositories/SpecificationRepository";
+import { CreateSpecificationUseCase } from "../modules/cars/useCases/createSpecification/CreateSpecificationUseCase";
+import { SpecificationRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationRoutes = Router();
 
-const specificationRepository = new SpecificationRepository();
-
 specificationRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationRoutes };
