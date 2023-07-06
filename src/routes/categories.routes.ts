e4bcd0930974c88b15b6e2f20797379dbea2f055
Bruna <brn.flor@gmail.com>
2023-07-06 20:38:47 +0100

    adding files csv

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 const categoriesRoutes = Router();
 
@@ -13,4 +18,12 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+
+  console.log(file);
+  return response.send();
+});
+
 export { categoriesRoutes };
